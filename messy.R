f78998ea247a92460a1f9a051115ad82c3427d7b
Your Name <kathypoch11@gmail.com>
2020-11-19 19:31:40 -0800

    renamed files

--- messy.R
+++ messy.R
@@ -4,22 +4,7 @@ library(mapproj)
 library(patchwork)
 incarceration <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 
-#grab texas data 
-texas_data <- incarceration %>%
-  select(aapi_jail_pop_rate, white_jail_pop_rate, black_jail_pop_rate,
-         total_jail_pop_rate, latinx_jail_pop_rate,
-         year, fips, state, county_name) %>% 
-  filter(state == "TX")
-
-# 2018 data of black and white people
-black_and_white_2018 <- incarceration %>%
-  select(white_jail_pop_rate, black_jail_pop_rate, total_jail_pop_rate,
-         year, fips, state, county_name) %>%
-  mutate(black_white_jail_ratio = black_jail_pop_rate / white_jail_pop_rate) %>%
-  filter(year == 2018)
-
-#### Summary ####
-
+#summary
 summary_info <- list()
 summary_info$num_observations <- nrow(incarceration) # number of rows
 summary_info$num_features <- ncol(incarceration) # number of columns
@@ -28,12 +13,12 @@ summary_info$num_features <- ncol(incarceration) # number of columns
 summary_info$highest_black_white_ratio <- black_and_white_2018 %>%
   filter(black_white_jail_ratio == max(black_white_jail_ratio, na.rm = T)) %>%
   pull(state)
-
+  
 #When did Texas have the highest rate of black people in jail?
-summary_info$highest_black_texas <- texas_data %>%
+summary_info$highest_black_texas_incarceration <- texas_data %>%
   filter(black_jail_pop_rate == max(black_jail_pop_rate, na.rm = T))%>%
   pull(year)
-
+  
 #Which state had the highest rate of latin people in jail of 2018?
 summary_info$highest_latin_incarceration <- incarceration %>%
   filter(year == 2018) %>%
@@ -47,12 +32,19 @@ summary_info$lowest_white_incarceration <- incarceration %>%
   pull(state)
 
 #Which state had the highest rate of Asian Americans in jail of 2018?
-summary_info$highest_aapi_incarceration <- incarceration %>%
+summary_info$highest_latin_incarceration <- incarceration %>%
   filter(year == 2018) %>%
   filter(aapi_jail_pop_rate == max(aapi_jail_pop_rate, na.rm = T)) %>%
   pull(state)
 
-#### My 2 variable scatter plot #####
+# 2018 data of black and white people
+black_and_white_2018 <- incarceration %>%
+  select(white_jail_pop_rate, black_jail_pop_rate, total_jail_pop_rate,
+         year, fips, state, county_name) %>% 
+  mutate(black_white_jail_ratio = black_jail_pop_rate / white_jail_pop_rate) %>%
+  filter(year == 2018)
+
+# My 2 variable scatter plot
 black_white_by_state <- ggplot(data = black_and_white_2018 %>% top_n(10)) +
   geom_point(mapping = aes(x = black_jail_pop_rate,
                            y = white_jail_pop_rate, col = state)) +
@@ -60,7 +52,7 @@ black_white_by_state <- ggplot(data = black_and_white_2018 %>% top_n(10)) +
        x = "Black Jail Population Rate", y = "White Jail Population Rate")
 
 
-##### My Map ####
+# My Map
 
 county_shapes <- map_data("county") %>%
   unite(polyname, region, subregion, sep = ",") %>%
@@ -69,6 +61,11 @@ county_shapes <- map_data("county") %>%
 map_data <- county_shapes %>%
   left_join(black_and_white_2018, by = "fips") %>%
   filter(state == "TX")
+
+# where the black/white ratio is > 1
+map_data1 <- map_data %>% 
+  mutate(black_white_jail_ratio = ifelse(black_white_jail_ratio > 1, black_white_jail_ratio, NA))
+View(map_data1)
 
 blank_theme <- theme_bw() +
   theme(
@@ -82,42 +79,68 @@ blank_theme <- theme_bw() +
     panel.border = element_blank()
   )
 
-blackwhite_ratio_map <- ggplot(map_data) +
+
+black_white_ratio_map <- ggplot(map_data1) +
   geom_polygon(
     mapping = aes(x = long, y = lat, group = group, fill = black_white_jail_ratio)) +
   coord_map() +
-  labs(title = "2018 Black to White Jail Population Ratio in Texas") +
-  labs(fill = "Black/White Ratio") +
+  labs(fill = "Black/White Ratio")+
+  labs(title = "Black to White Jail Population Ratio in Texas") +
   blank_theme
 
-#### My Trend Over Time ####
+#where the black/white ratio is < 1  
+map_data2 <- map_data %>% 
+  mutate (black_white_jail_ratio = ifelse(black_white_jail_ratio < 1, black_white_jail_ratio, NA))
+View(map_data2)
 
-#Grab one county data
+white_greater <- ggplot(map_data2) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = black_white_jail_ratio)) +
+  blank_theme +
+  ggtitle("White incarceration rate > Black incarceration rate") +
+  coord_map() +
+  labs(fill = "Black/White Ratio")
+
+comparison_plots <- black_white_ratio_map/white_greater
+
+#Trends chart Over Time 
+
+#grab texas data 
+texas_data <- incarceration %>%
+  select(aapi_jail_pop_rate, white_jail_pop_rate, black_jail_pop_rate,
+         total_jail_pop_rate, latinx_jail_pop_rate,
+         year, fips, state, county_name) %>% 
+  filter(state == "TX")
+
+texas_data <- texas_data %>% group_by(year) %>% ungroup()
+
+black_and_white_2018 %>% filter(year == 2018) %>%
+  filter(state == "TX") %>% filter(black_white_jail_ratio == max(black_white_jail_ratio, na.rm = T)) %>% 
+  select(county_name, black_white_jail_ratio
+)
+
+length(unique(texas_data$year))
+
+
 terrell_county <- texas_data %>%
   filter(county_name == "Terrell County")
 terrell_county2 <- terrell_county[-(1:20),]
+  
+
 
 library(reshape2)
 
 meltdf <- melt(terrell_county2, id = "year", measure = c("white_jail_pop_rate",
                                                         "latinx_jail_pop_rate"))
-latin_white <- ggplot(meltdf, aes(year, value, colour = variable)) +
+
+ggplot(meltdf, aes(year, value, colour = variable)) +
   geom_line() +
-  labs(title = "Terrell County White vs. Latin Incarceration Rates",
+  labs(title = "Terrell County, Texas Incarceration Rates by Race",
        x = "year", y = "Jail Population Rate", color = "Race") +
   scale_color_hue(labels = c("White", "Latin"))
 
-meltdf2 <- melt(terrell_county2, id = "year", measure = c("white_jail_pop_rate",
-                                                        "black_jail_pop_rate"))
-
-black_white <- ggplot(meltdf2, aes(year, value, colour = variable)) +
+ggplot(meltdf, aes(year, value, colour = variable)) +
   geom_line() +
-  labs(title = "Terrell County White vs. Black Incarceration Rates",
+  labs(title = "Terrell County, Texas Incarceration Rates by Race",
        x = "year", y = "Jail Population Rate", color = "Race") +
-  scale_color_hue(labels = c("White", "Black"))
-
-comparing_trends <- black_white / latin_white
-
-
-
-
+  scale_color_hue(labels = c("White", "Latin", "Asian"))
